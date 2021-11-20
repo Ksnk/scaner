236dc3cc0aa1376904b9e907a351b545570049e7
ksnk <sergekoriakin@gmail.com>
2021-11-20 19:07:53 +0300

    Добавлены дочерние классы сканера

---
+++ src/csv.php
@@ -0,0 +1,204 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: s.koryakin
+ * Date: 22.11.2019
+ * Time: 17:10
+ */
+
+namespace Ksnk\scaner;
+
+/**
+ * Class csv
+ * @package Ksnk\scaner
+ * читатель csv, возможно в не той кодировке, возможно с не теми делимитерами
+ * и не теми обрамлениями, сам все найдет и прочитает как надо.
+ * кодировки понимает только cp1251 и utf-8, но в моей реальности этого хватает.
+ */
+class csv extends scaner
+{
+
+  const BOM = "\xEF\xBB\xBF"; // utf-8
+  const LE16 = "\xFF\xFE"; // utf-16LE на будущее. Армагедон не за горами ?
+
+  var $delim = ';',
+    $quote = '"',
+    $hasbom = 0,
+    $newline = '',
+    $detected = false,
+    $encoding = 'windows-1251',
+    $_reg;
+
+  function param($array)
+  {
+    foreach ($array as $k => $v) {
+      if (isset($this->{$k}))
+        $this->{$k} = $v;
+    }
+  }
+
+  /**
+   * Упрощенная генерация класса для распарсинга строки
+   * @param $string
+   * @param array $param
+   * @return csv
+   *
+   * ключи параметра
+   * 'delim','quote','encoding','detected'
+   * Для вставки одного столбца из Excel -  [delim:"\t",]
+   */
+  static function csvStr($string, $param = [])
+  {
+    $class = new self();
+    if(!preg_match('/[\n\r]$/',$string))$string.="\n";
+    $class->newbuf($string);
+    if (!isset($param['encoding'])) {
+      $param['encoding'] = mb_detect_encoding($string, 'utf-8', true)
+        ? 'utf-8' : 'cp1251';
+    }
+    $class->param($param);
+    return $class;
+  }
+
+  /**
+   * функция выдает класс csv с правильно определенной кодировкой и делимитерами
+   * @param $nameresource
+   * @param $headers - количество строк на заголовок.
+   * @return csv
+   */
+  static function getcsv($nameresource, $headers = 1)
+  {
+    $class = new self();
+    $class->newhandle($nameresource);
+
+    $class->prepare();
+    $buf = $class->getbuf();
+    if ($encoding = mb_detect_encoding($buf, 'utf-8', true)) {
+        $class->param(['encoding' => $encoding]);
+    }
+    // первые 2-3 символа - не БОМ?
+    if (preg_match('~^(' . self::BOM . '|' . self::LE16 . ')~', $buf, $m)) {
+      if ($m[0] == self::BOM) {
+        $class->param(['encoding' => 'utf-8']);
+        $class->hasbom = 3;
+        $class->position(3);
+      } else {
+        $encoding = 'utf-16RLE';//todo сделать ?
+        $class->hasbom = 2;
+        $class->position(2);
+      }
+    }
+    $the_start = $class->getpos();
+    foreach (['"', "'"] as $quote) foreach ([',', ";", "\t"] as $delim) {
+      $start = $the_start;
+      $cols = 0;
+      $rows = 0;
+      $row = 0;
+      $line = str_replace('"', $quote,
+        str_replace(',', $delim,
+          '~(?:(")(?:[^"]|"")*"|.*?)(,|(\r\n|\n|\r))()~s'));
+      //if ($encoding == 'utf-8') $line .= 'u';
+      while (preg_match($line, $buf, $m, PREG_OFFSET_CAPTURE, $start)) {
+        if ($start != $m[0][1]) {
+          // пропустили кусок текста, проблемы, однако
+          continue 2;
+        }
+        if ($m[1][1] > 0 && $quote != $m[1][0]) {
+          // не подходит QUOTE
+          continue 2;
+        }
+        if ($m[3][1] > 0) {
+          // so newline
+          if ($cols == 0 && $row == 0) {
+            // количество столбцов разное
+            continue 2;
+          } else if ($cols == 0) $cols = $row;
+          else if ($cols !== $row) {
+            // количество столбцов разное
+            continue 2;
+          }
+          $row = 0;
+          $start = $m[4][1];
+          if ($rows++ > 6 || $start > scaner::BUFSIZE >> 1 || $start >= $class->finish) {
+            // проверка пройдена, заканчиваем
+            $class->param([
+              'delim' => $delim,
+              'quote' => $quote,
+              'encoding' => $encoding,
+              'detected' => true
+            ]);
+            break 3;
+          }
+          continue;
+        } else {
+          $start = $m[4][1];
+          if ($delim != $m[2][0]) {
+            // не подходит DELIM.
+            continue 2;
+          }
+        }
+        $row++;
+      }
+    }
+    return $class;
+  }
+
+
+  function nextRow()
+  {
+    $key = '';
+    if (empty($this->_reg)) {
+      $this->_reg0 = str_replace('"', $this->quote,
+        str_replace(',', $this->delim, '~((")(?:[^"]|"")*"|.*?)(,|(\r\n|\n|\r))()~s'));
+      $this->_reg = str_replace('"', $this->quote,
+        str_replace(',', $this->delim, '~(.*?)(,|(\r\n|\n|\r))()~s'));
+      if ($this->encoding == 'utf-8') {
+        $this->_reg0.= 'u';
+        $this->_reg.= 'u';
+      }
+    }
+    $this->prepare(false);
+    if ($this->finish - $this->filestart - $this->start <= 0) return [];
+    $cols = [];
+    $m = [];
+    while (true) {
+      if (preg_match($this->_reg0 , $this->buf, $m, PREG_OFFSET_CAPTURE, $this->start)) {
+        $col = preg_replace(['/^' . $this->quote . '/', '/' . $this->quote . '$/', '/' . $this->quote . $this->quote . '/'], ['', '', $this->quote], $m[1][0]);
+        array_shift($m);
+      } else // иногда, из за большого объема данных в поле, регулярка не выедает все в первый раз
+          if(isset($this->buf{$this->start}) && $this->buf{$this->start} == $this->quote ||
+          preg_match($this->_reg , $this->buf, $m, PREG_OFFSET_CAPTURE, $this->start)) {
+          if (empty($m)) {
+            // попытка прочитать поврежденный текст
+            $this->start++;
+            $col = '';
+            while (preg_match('~[^' . $this->quote . ']*' . $this->quote . '()~s' . $key, $this->buf, $m, PREG_OFFSET_CAPTURE, $this->start)) {
+              $this->start = $m[1][1];
+              if ($this->buf{$this->start} == $this->quote) {
+                $this->start++;
+                $col .= $m[0][0];
+              } else {
+                $col .= mb_substr($m[0][0], 0, -1, '8bit');
+                break;
+              }
+            }
+            preg_match('~()(' . $this->delim . '|(\r\n|\n|\r))()~su', $this->buf, $m, PREG_OFFSET_CAPTURE, $this->start);
+          } else {
+            $col = $m[1][0];
+          }
+        }
+      if ($this->encoding != 'utf-8') $col = iconv($this->encoding, 'utf-8//ignore', $col);
+      $cols[] = $col;
+      if(empty($m)){
+        break;
+      } else {
+        $this->start = $m[4][1];
+        if ($m[3][1] >= 0) {
+          break;
+        }
+      }
+      $m = [];
+    }
+    return $cols;
+  }
+}
---
+++ src/spider.php
@@ -0,0 +1,261 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ksnk
+ * Date: 24.11.15
+ * Time: 18:52
+ */
+
+namespace Ksnk\scaner;
+
+/**
+ * Паучёк
+ * file_get_contents ===
+ *   spider::getcontents($url)
+ * Class spider
+ */
+class spider extends scaner
+{
+
+    var $curl,
+//        $debug = 0,
+        $siteroot,
+        $cookie_file = 'tmp_cookie.txt',
+        $pclzip_path,
+        $info=[];
+
+    var $lasturl = array(
+        'scheme' => 'http',
+        'host' => '',
+        'user' => '',
+        'pass' => '',
+        'path' => '',
+        'query' => '',
+        'fragment' => '',
+    );
+
+    var $stat = [
+        'redirects' => [],
+    ];
+
+    private function curl($url, $opt = array())
+    {
+        try {
+
+            $ch = curl_init();
+            $opt = [
+                    'level' => 1
+                ] + $opt;
+            $this->siteroot = $this->buildurl($url, true);
+            $redirect = '';
+            if ($this->debug()) {
+                $verbose = fopen('php://temp', 'w+');
+                curl_setopt($ch, CURLOPT_VERBOSE, true);
+                curl_setopt($ch, CURLOPT_STDERR, $verbose);
+            }
+            if (empty($opt['level'])) {
+                // Минимальная маскировка, не меняем useragent
+                $user_agent = 'Kadis Bot/1.0';
+            } else if ($opt['level'] > 0) {
+                $user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36';
+            }
+            $header = array(
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+                "Accept-Encoding: gzip, deflate",
+                "Accept-Language: ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Upgrade-Insecure-Requests: 1",
+                //           "Keep-Alive: 300"
+            );
+
+            curl_setopt($ch, CURLOPT_URL, $this->siteroot); // set url to post to
+            if (\UTILS::val($opt, 'method') == 'POST') {
+                curl_setopt($ch, CURLOPT_POST, 1);
+                $data = \UTILS::val($opt, 'data', []);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            } else if (\UTILS::val($opt, 'method') == 'HEAD') {
+                curl_setopt($ch, CURLOPT_NOBODY, true);
+            }
+            curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // allow redirects
+            curl_setopt($ch, CURLOPT_ENCODING, 'gzip');
+            //curl_setopt($ch, CURLOPT_HTTP_VERSION,'1.0');
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_USERAGENT, $user_agent);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 60); // times out after 4s
+            curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+            // curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+            $x = curl_exec($ch);
+            if ($this->debug()) {
+                rewind($verbose);
+                $this->debug(stream_get_contents($verbose));
+            }
+            $info = curl_getinfo($ch);
+            $this->info=$info;
+            $this->debug("%s\n", $info);
+            if (false === $x) {
+                $this->error("Ошибка curl: %s\n", curl_error($ch));
+                $this->newbuf(''); // run the whole process
+            } else {
+                $content_type = $info['content_type'];
+                if (preg_match('/charset=\s*(.+)\s*/', $content_type, $found)) {
+                    $p = strtoupper(trim($found[1]));
+                    if ($p != 'UTF-8') $x = mb_convert_encoding($x, 'UTF-8', $p);
+                };
+                $this->newbuf($x); // run the whole process
+                $x = $this->buildurl($info['url'], true);
+                /*  if ($x != $this->siteroot) {
+                    $this->out("`%s` => `%s`\n",$this->siteroot,$x);
+                  } else {
+                    $this->out("%s\n", $this->siteroot);
+                  } */
+                if ($info['http_code'] == 302) {
+                    $this->stat['redirects'][$url] = $info['url'];
+                    $redirect = $info['redirect_url'];
+                }
+            }
+
+            if ($this->debug()) {
+                $version = curl_version();
+                $this->debug('
+    date: %s
+    %s
+    URL....: %s
+    Code...: %s (%s redirect(s) in %s secs)
+    Content: %s Size: %s (Own: %s) Filetime: %s
+    Time...: %s Start @ %s (DNS: %s Connect: %s Request: %s)
+    Speed..: Down: %s (avg.) Up: %s (avg.)
+    Curl...: v{%s}
+    ',
+                    date('Y-m-d H:i:s T'), $x,
+                    $info['url'], $info['http_code'], $info['redirect_count'],
+                    $info['redirect_time'],
+                    $info['content_type'], $info['download_content_length'],
+                    $info['size_download'], $info['filetime'],
+                    $info['total_time'], $info['starttransfer_time'],
+                    $info['namelookup_time'], $info['connect_time'],
+                    $info['pretransfer_time'], $info['speed_download'],
+                    $info['speed_upload'], $version['version']
+                );
+            }
+            curl_close($ch);
+            if (isset($opt['callback']) && is_callable($opt['callback'])) {
+                $callback = $opt['callback'];
+                $callback($info, $x);
+            }
+            if (!empty($redirect)) {
+                $this->curl($redirect);
+            }
+        } catch (\Exception $e) {
+            $this->error("curl: ".$url."\n".'Exception: '.$e->getMessage());
+            $this->newbuf('');
+            if(empty($this->info['status']))
+                $this->info['status']='404';
+        }
+        return $this;
+    }
+
+    /**
+     * Build url from last opened url's
+     * @param $url
+     * @param bool $update
+     * @return string
+     */
+    function buildurl($url, $update = false)
+    {
+        $parsed = parse_url(trim($url));
+        // собираем url обратно
+        $urls = '';
+        if (!isset($parsed['scheme'])) {
+            $parsed['scheme'] = $this->lasturl['scheme'];
+            $parsed['host'] = $this->lasturl['host'];
+        }
+        // idna
+
+        if (!isset($parsed['host'])) {
+            return $url;
+        }
+
+        $host_utf = \UTILS::idn_to_utf8($parsed['host']);
+        $host_loc = \UTILS::idn_to_ascii($parsed['host']);
+
+        if (!empty($host_utf) && $host_utf != $parsed['host']) {
+            $parsed['host'] = $host_utf;
+        } elseif (!empty($host_loc) && $host_loc != $parsed['host']) {
+            $parsed['host'] = $host_loc;
+        }
+        if (!empty($parsed['path']) && $parsed['path']{0} != '/') {
+            $parsed['path'] = preg_replace('~[^/]*$~', '', $this->lasturl['path']) . $parsed['path'];
+        }
+        foreach (['scheme' => '%s:', 'host' => '//%s', 'path' => '%s', 'query' => '?%s'] as $k => $v) {
+            if (!empty($parsed[$k])) $urls .= sprintf($v, $parsed[$k]);
+        }
+
+        if ($update) {
+            $this->lasturl = $parsed;
+        }
+        return $urls;
+    }
+
+    /**
+     * Имя сайта
+     */
+    function open($url, $opt = array())
+    {
+        $this->curl($url, $opt);
+        return $this;
+    }
+
+    function uploadfile($url, $img, $_archive)
+    {
+        $this->curl($url);
+        require_once $this->pclzip_path . 'pclzip.lib.php';
+        $archive = new PclZip($_archive);
+        if (is_readable($_archive)) {
+            $list = $archive->add(array(
+                    array(PCLZIP_ATT_FILE_NAME => $img,
+                        PCLZIP_ATT_FILE_CONTENT => $this->buf
+                    )
+                )
+            );
+        } else {
+            $list = $archive->create(array(
+                    array(PCLZIP_ATT_FILE_NAME => $img,
+                        PCLZIP_ATT_FILE_CONTENT => $this->buf
+                    )
+                )
+            );
+        }
+        if ($list == 0) {
+            die("ERROR : '" . $archive->errorInfo(true) . "'");
+        }
+
+        $this->newbuf('');
+        return $this;
+    }
+
+    /**
+     * Имя сайта
+     */
+    function post($url, $data)
+    {
+        return $this->curl($url, [
+            'method' => 'POST',
+            'data' => $data
+        ]);
+
+    }
+
+    function getcontents($url)
+    {
+        $this->debug = 1;
+        $this->curl($url);
+        return $this->buf;
+    }
+}
