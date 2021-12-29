cc8b2058e52cf510dd73ef5e70ebd9da8091d32a
Ksnk <sergekoriakin@gmail.com>
2021-12-29 17:57:19 +0300

    Тесты обновлены

--- tests/certTest.php
+++ tests/certTest.php
@@ -30,42 +30,67 @@ class certTest extends TestCase
                 )
             )
         );
+        try {
+            $fp = stream_socket_client($url, $err_no, $err_str, 2, STREAM_CLIENT_CONNECT, $context);
+            if (!!$fp) {
+                $cert = stream_context_get_params($fp);
 
-        $fp = stream_socket_client($url, $err_no, $err_str, 30, STREAM_CLIENT_CONNECT, $context);
-        $cert = stream_context_get_params($fp);
-
-        if (empty($err_no)) {
-            $info = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
-            //print_r($info);
+                if (empty($err_no)) {
+                    $info = openssl_x509_parse($cert['options']['ssl']['peer_certificate']);
+                    //print_r($info);
+                    return $info;
+                }
+            }
+            return false;
+        } catch(\Exception $e){
+            return false;
         }
-        return $info;
     }
 
     function test_cert()
     {
+        $results=[];
+        $fault=0;
         foreach([
-            'new.snabworld.ru','eco-bakaleya.ru',
+           // 'new.snabworld.ru',
+           // 'eco-bakaleya.ru',
             'kadis.org','consultant.kadis.org','info.kadis.org',
             'outportal.kadis.org',
             'audit.kadis.org',
-            'кадис-аудит.рф','express.kadis.org',
-            'buhgalteru.kadis.org',
+            'arbspor.ru',
+            'кадис-аудит.рф','express.kadis.org','meeting.kadis.org','events.kadis.org',
+            'land.kadis.org',
         ] as $host) {
+            $str = '----------------------------------' . "\r\n" ;
             $info = $this->getCert($host);
-            echo '----------------------------------' . "\r\n";
-            echo 'Домен: ' . $info['subject']['CN'] . "\r\n";
-            echo 'Выдан: ' . $info['issuer']['CN'] . "\r\n";
-            if(time()>$info['validTo_time_t']){
-                echo 'Просрочен: ' . date('d.m.Y H:i', $info['validTo_time_t']);
+            if(!$info){
+                $fault++;
+                $str .= 'Домен: ' . $host . "\r\n" ;
+                $str .= 'не доступен ' . "\r\n";
+                $results[$str]=0;
             } else {
-                $t = new \DateTime();
-                $t->setTimestamp($info['validTo_time_t']);
-                $interval = date_create('now')->diff( $t );
-                echo $interval->format('Истекает через %R%a дней  %H').' - ' . date('d.m.Y H:i', $info['validTo_time_t']);
+                if($host!=$info['subject']['CN']) {
+                    $str .= 'Домен: ' .$host.'('. $info['subject']['CN'] . ")\r\n";
+                } else {
+                    $str .= 'Домен: ' . $info['subject']['CN'] . "\r\n";
+                }
+                $str .=  'Выдан: ' . $info['issuer']['CN'] . "\r\n";
+                if (time() > $info['validTo_time_t']) {
+                    $fault++;
+                    $str .= 'Просрочен: ' . date('d.m.Y H:i', $info['validTo_time_t']);
+                } else {
+                    $t = new \DateTime();
+                    $t->setTimestamp($info['validTo_time_t']);
+                    $interval = date_create('now')->diff($t);
+                    $str .= $interval->format('Истекает через %R%a дней  %H') . ' - ' . date('d.m.Y H:i', $info['validTo_time_t']);
+                }
+                $str.="\r\n";
+                $results[$str]=$info['validTo_time_t'];
             }
-            echo "\r\n";
         }
-
+        asort($results);
+        echo implode('',array_keys($results));
+        $this->assertEquals(0, $fault);
     }
 
 }
